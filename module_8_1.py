551f8a3975edbcce13a2c22773779f249c7eef4a
santfan <santfan@mail.ru>
2024-11-12 17:58:24 +0300

    Задание по теме Try-Except

--- module_8_1.py
+++ module_8_1.py
@@ -1,15 +1,20 @@
+# Создадим функцию которая принимает два параметра
 def add_everything_up(a, b):
+  # Тут возможно возникновение ошибок
   try:
+    # Если возможно то вернем сумму
     return a + b
+  # Если случится ошибка типа TypeError
   except TypeError:
+    # Если нельзя сложить раные типы, преобразуем типы
     return str(a) + str(b)
-
+# Подадаим число и строку (будет ошибка)
 print(add_everything_up(1234, 'string'))
-
+# Подадим два числа (ошибок нет)
 print(add_everything_up(1, 7))
-
+# Подадим строку и число (будет ошибка)
 print(add_everything_up('duple', 17))
-
+# Подадим две строки (ошибок нет)
 print(add_everything_up('Привет ', 'незнакомец'))
 
 print(add_everything_up(12.3567, 344.5))
